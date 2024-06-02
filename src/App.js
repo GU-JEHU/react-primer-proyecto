f812f96140d533f53af98b643cf758823ecd7480
GU-JEHU <jehu.desarrollo@gmail.com>
2024-06-01 18:50:31 -0600

    Se agrego el esqueleto inicial para la aplicacion de Todo List y se borro el codigo anterior.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoCounter/>
+      <TodoSearch/>
+
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
+      
+      <CreateTodoButtom/>
     </div>
+  );
+}
+
+function TodoItem(){
+  return(
+    <li>
+      <span>V</span>
+      <p>A llorar a la lloreria</p>
+      <span>X</span>
+    </li>
   );
 }
 
